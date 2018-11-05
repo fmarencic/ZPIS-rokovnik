8f2e76ce8e2077092b6a76698cc030b72cac5d50
jbijelic <jbijelic@foi.hr>
2018-11-05 22:50:46 +0100

    no message

---
+++ databaseAzure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace databaseAzure
+{
+    public class ZPISRokovnikDatabaseContext : DbContext
+    {
+        private readonly string databasePath;
+
+
+    }
+}
