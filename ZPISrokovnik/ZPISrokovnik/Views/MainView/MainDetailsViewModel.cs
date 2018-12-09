e8a8d730a6103e98a96aa53e62e68aff3f3adc97
Filip Marenčić <fmarencic@gmail.com>
2018-12-09 15:37:28 +0100

    Završen izgled MainDetails View-a, dodana generična slika za prikaz osobe bez slike

--- ZPISrokovnik/ZPISrokovnik/Views/MainView/MainDetailsViewModel.cs
+++ ZPISrokovnik/ZPISrokovnik/Views/MainView/MainDetailsViewModel.cs
@@ -10,11 +10,25 @@ namespace ZPISrokovnik.Views.MainView
 {
 	public class MainDetailsViewModel : BaseViewModel
 	{
-		public MainDetailsViewModel (IPageService page)
+        #region Constructor
+        public MainDetailsViewModel (IPageService page)
 		{
             this.pageService = page;
 		}
+        #endregion
 
+        #region Properties
+        private string caption = "Podaci o osobi";
         private IPageService pageService;
-	}
+
+        public string Caption
+        {
+            get { return caption; }
+            set {
+                caption = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
 }
