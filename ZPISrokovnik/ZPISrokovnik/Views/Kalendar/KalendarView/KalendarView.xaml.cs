81ba0a0b49bcb19d19ce7d7bbbef8e3f05da1e37
Luka Ljubičić <lljubici1@foi.hr>
2018-12-09 17:13:38 +0100

    Napravljen KalendarView Napravljen view za unos događaja u kalendar

---
+++ ZPISrokovnik/ZPISrokovnik/Views/Kalendar/KalendarView/KalendarView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ZPISrokovnik.Views.Kalendar.KalendarView
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class KalendarView : ContentPage
+	{
+		public KalendarView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ ZPISrokovnik/ZPISrokovnik/Views/Kalendar/UnesiNapomenuView/UnesiNapomenuModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZPISrokovnik.Views.Kalendar
+{
+    public class UnesiNapomenuModel
+    {
+        public DateTime Datum { get; set; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+
+        public UnesiNapomenuModel()
+        {
+            
+        }
+
+        public UnesiNapomenuModel(DateTime datum, string naziv, string opis)
+        {
+            this.Datum = datum;
+            this.Naziv = naziv;
+            this.Opis = opis;
+        }
+    }
+}
