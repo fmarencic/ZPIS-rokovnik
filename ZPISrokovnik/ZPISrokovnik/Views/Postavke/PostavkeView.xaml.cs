61a5f804360370f91ad829f75fcb1723725f13cd
kolaricmatej <34926004+kolaricmatej@users.noreply.github.com>
2018-12-21 17:25:14 +0100

    Kreiran PostavkeView designe view unutar kojeg je potrebno kreirati logiku

---
+++ ZPISrokovnik/ZPISrokovnik/Views/Postavke/PostavkeView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ZPISrokovnik.Views.Postavke
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostavkeView : ContentPage
+    {
+        public string Korisnik { get; set; }
+        public string Lozinka{get; set; }
+        public PostavkeView()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnClicked(object sender, EventArgs eventArgs)
+        {
+            await Navigation.PushAsync(new PromjenaLozinkeView());
+        }
+        
+    }
+}
