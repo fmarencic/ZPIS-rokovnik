1ed3512616ea89d1f939be3cf7b62f85a96866b2
jbijelic <jbijelic@foi.hr>
2019-01-16 17:11:48 +0100

    Klasa za bojanje neispravnih zapisa.

---
+++ ZPISrokovnik/ZPISrokovnik.Android/Utils/EntryLineColorEffect.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using ZPISrokovnik.Behaviors;
+
+namespace ZPISrokovnik.Droid.Utils
+{
+    public class EntryLineColorEffect : PlatformEffect
+    {
+        EditText control;
+
+        protected override void OnAttached()
+        {
+            try
+            {
+                control = Control as EditText;
+                UpdateLineColor();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            control = null;
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == LineColorBehavior.LineColorProperty.PropertyName)
+            {
+                UpdateLineColor();
+            }
+        }
+
+        private void UpdateLineColor()
+        {
+            try
+            {
+                if (control != null)
+                {
+                    control.Background.SetColorFilter(LineColorBehavior.GetLineColor(Element).ToAndroid(), Android.Graphics.PorterDuff.Mode.SrcAtop);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
