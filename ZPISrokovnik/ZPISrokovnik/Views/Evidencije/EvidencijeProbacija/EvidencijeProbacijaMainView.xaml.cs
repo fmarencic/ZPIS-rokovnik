16a9c8201d47d134167e20faa2adb9eafd43d0e5
Filip Marenčić <fmarencic@gmail.com>
2019-01-29 17:18:12 +0100

    Kreiran View evidencije za korisnike probacije

---
+++ ZPISrokovnik/ZPISrokovnik/Views/Evidencije/EvidencijeProbacija/EvidencijeProbacijaMainView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZPISrokovnik.Utils;
+
+namespace ZPISrokovnik.Views.Evidencije.EvidencijeProbacija
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EvidencijeProbacijaMainView : ContentPage
+	{
+		public EvidencijeProbacijaMainView ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new EvidencijeProbacijaMainViewModel(new PageService());
+		}
+	}
+}
